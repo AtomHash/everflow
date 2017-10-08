ae9b2abb460ef61dbc3f9a9a8eb141e1895cbbf5
Dylan Harty <me@dylanharty.com>
2017-10-08 14:16:48 -0600

    added Translator.convertPage(), finds trans("dotKey") to translate certain element properties.

--- dist/types/everflow/translator.d.ts
+++ dist/types/everflow/translator.d.ts
@@ -4,4 +4,6 @@ export default class Translator {
     private static loadData(key);
     private static listKey(key);
     static trans(dotKey?: string): string;
+    static convertPage(): void;
+    private static convertProperty(element, property);
 }
--- everflow/translator.ts
+++ everflow/translator.ts
@@ -32,5 +32,46 @@ export default class Translator
         }
         return currentSelection;
     }
+    
+    static convertPage()
+    {
+        var elements = document.getElementsByTagName("*");
+        var checkProperties =
+        [
+            'placeholder',
+            'innerText',
+            'defaultValue'
+        ];
+        for (var i = 0, max = elements.length; i < max; i++)
+        {
+            for (var checkIndex in checkProperties) {
+                var check = checkProperties[checkIndex];
+                var element = elements[i];
+                if (check in element)
+                {
+                    Translator.convertProperty(element, check);
+                }
+            }
+        }
+
+    }
+    
+    private static convertProperty(element, property)
+    {
+        var value = element[property];
+        if (Utils.isEmpty(value))
+        {
+            return;
+        }
+        var expr = /trans\('([\w.]+)'\)/g;
+        var matchList = value.match(expr);
+        for (var index in matchList) {
+            var exprKey = /'([\w.]+)'/;
+            var match = matchList[index];
+            var key = match.match(exprKey)[1];
+            element[property] = Translator.trans(key);
+        }
+
+    }
 
 }
