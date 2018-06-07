3e4fcc612b49f09d4256517db7780b2b4ae8e809
Dylan Harty <me@dylanharty.com>
2018-06-07 16:44:31 -0600

    added disableAll() to Request to disable all buttons that can submit to the server.

--- everflow/request.ts
+++ everflow/request.ts
@@ -6,17 +6,18 @@ import errors from './errors/--init--';
  * Creates an Everflow Request object. Intergrates Axios into the Everflow framework.
  * @class
  */
-export default class Request 
-{
-    config: any = window.app.config;
-    endPoint:string = '';
-    authorize: boolean = false;
-    method: string = '';
-    data: any = null;
-    responseType: string = 'json';
-    headers: object = {};
-    retries: number = 0;
-    maxTime: number = 8000;
+ export default class Request 
+ {
+     config: any = window.app.config;
+     endPoint:string = '';
+     authorize: boolean = false;
+     method: string = '';
+     data: any = null;
+     responseType: string = 'json';
+     headers: object = {};
+     retries: number = 0;
+     maxTime: number = 8000;
+     disableAllButtons = false;
 
     /**
      * Initializes the Request
@@ -24,11 +25,11 @@ export default class Request
      * @param {string} url - request url
      * @param {boolean} authorize - if true add JWT to request 
      */
-    constructor(url: string, authorize: boolean = false)
-    {
-        this.endPoint = url;
-        this.authorize = authorize;
-    }
+     constructor(url: string, authorize: boolean = false)
+     {
+         this.endPoint = url;
+         this.authorize = authorize;
+     }
 
     /**
      * Perform multiple requests and end at the same time.
@@ -36,10 +37,10 @@ export default class Request
      * @param {Array<AxiosPromise>} requests - multiple Requests to handle
      * @static
      */
-    static multiple(requests: Array<AxiosPromise>): Promise<AxiosResponse<any>[]>
-    {
-        return Axios.all(requests)
-    }
+     static multiple(requests: Array<AxiosPromise>): Promise<AxiosResponse<any>[]>
+     {
+         return Axios.all(requests)
+     }
 
     /**
      * Axios spread function to handle mutiple response callbacks
@@ -47,20 +48,20 @@ export default class Request
      * @param {callable} callback - callback with a paramter for each response in Request.multiple([])
      * @static
      */
-    static spread(callback): (array: {}[]) => {}
-    {
-        return Axios.spread(callback)
-    }
+     static spread(callback): (array: {}[]) => {}
+     {
+         return Axios.spread(callback)
+     }
 
     /**
      * Get Axios default engine
      * @function getAxiosEngine
      * @static
      */
-    static getAxiosEngine(): AxiosInstance
-    {
-        return Axios
-    }
+     static getAxiosEngine(): AxiosInstance
+     {
+         return Axios
+     }
 
     /**
      * Add header to Request
@@ -68,85 +69,95 @@ export default class Request
      * @param {string} name - name of header
      * @param {string} value - value of header
      */
-    addHeader(name, value): Request
-    {
-        this.headers[name] = value
-        return this;
-    }
+     addHeader(name, value): Request
+     {
+         this.headers[name] = value
+         return this;
+     }
 
     /**
      * Retry the Request on failure(HTTP 500)
      * @function retry
      * @param {string} retries - default 3
      */
-    retry(retries: number = 3): Request
-    {
-        this.retries = retries;
-        return this;
-    }
+     retry(retries: number = 3): Request
+     {
+         this.retries = retries;
+         return this;
+     }
+
+     /**
+     * Disable all buttons on page while in-progress
+     * @function disableAll
+     */
+     disableAll()
+     {
+         this.disableAllButtons = true;
+         return this;
+     }
 
     /**
      * Timeout in miliseconds before the Request is terminated
      * @function timeout
      * @param {string} miliseconds - default 8000 = 8s
      */
-    timeout(miliseconds: number = 8000): Request
-    {
-        this.maxTime = miliseconds;
-        return this;
-    }
+     timeout(miliseconds: number = 8000): Request
+     {
+         this.maxTime = miliseconds;
+         return this;
+     }
 
     /**
      * Set Request to HTTP method of GET and execute
      * @function get
      */
-    get(): AxiosPromise
-    {
-        this.method = 'GET';
-        return this.build();
-    }
+     get(): AxiosPromise
+     {
+         this.method = 'GET';
+         return this.build();
+     }
 
     /**
      * Set Request to HTTP method of DELETE and execute
      * @function delete
      */
-    delete(): AxiosPromise
-    {
-        this.method = 'DELETE';
-        return this.build();
-    }
+     delete(): AxiosPromise
+     {
+         this.method = 'DELETE';
+         return this.build();
+     }
 
     /**
      * Set Request to HTTP method of PUT and execute
      * @function put
      * @param {object} data - data to be sent to server
      */
-    put(data: any): AxiosPromise
-    {
-        this.method = 'PUT';
-        this.data = data;
-        return this.build();
-    }
+     put(data: any): AxiosPromise
+     {
+         this.method = 'PUT';
+         this.data = data;
+         return this.build();
+     }
 
     /**
      * Set Request to HTTP method of POST and execute
      * @function post
      * @param {object} data - data to be sent to server
      */
-    post(data: any): AxiosPromise
-    {
-        this.method = 'POST';
-        this.data = data;
-        return this.build();
-    }
+     post(data: any): AxiosPromise
+     {
+         this.method = 'POST';
+         this.data = data;
+         return this.build();
+     }
 
     /**
      * Set Request to HTTP method of PATCH and execute
      * @function patch
      * @param {object} data - data to be sent to server
      */
-    patch(data: any): AxiosPromise
-    {
+     patch(data: any): AxiosPromise
+     {
         //Display.loader.on();
         this.method = 'PATCH';
         this.data = data;
@@ -158,13 +169,13 @@ export default class Request
      * @function build
      * @private
      */
-    private build() : AxiosPromise
-    {
-        var url: string = '';
-        if (this.authorize)
-        {
-            this.addHeader('Authorization', `Bearer ${window.app.user.token}`)
-        }
+     private build() : AxiosPromise
+     {
+         var url: string = '';
+         if (this.authorize)
+         {
+             this.addHeader('Authorization', `Bearer ${window.app.user.token}`)
+         }
         //added to support external links
         if (_.startsWith(this.endPoint.toLowerCase(), 'http'))
         {
@@ -217,6 +228,31 @@ export default class Request
         }
         ax.interceptors.response.use(undefined, iRetryFunction);
         ax.interceptors.request.use(undefined, iRetryFunction);
+        if (this.disableAllButtons)
+        {
+            var disableAllButtonsFunction = function(disabled)
+            {
+                var buttons = document.querySelectorAll("input[type=button], button[type=submit], button[type=button], input[type=submit]");
+                for(var i = 0, len = buttons.length; i < len; i++)
+                {
+                    buttons[i]['disabled'] = disabled;
+                }
+            }
+            ax.interceptors.request.use(function (config) {
+                disableAllButtonsFunction(true);
+                return config;
+            }, function (error) {
+                disableAllButtonsFunction(false);
+                return Promise.reject(error);
+            });
+            ax.interceptors.response.use(function (response) {
+                disableAllButtonsFunction(false);
+                return response;
+            }, function (error) {
+                disableAllButtonsFunction(false);
+                return Promise.reject(error);
+            });
+        }
         // add retires to config
         config['retries'] = self.retries;
         return ax[`${config.method.toLowerCase()}`](config.url, config)
